aa31e1b700825d630a7ba9ecec4c0a16ad6e182a
gattyan27 <radio.radirer@gmail.com>
2024-04-30 22:40:07 +0900

    first commit

---
+++ server/go.mod
@@ -0,0 +1,8 @@
+module github.com/gattyan27/go-grpc
+
+go 1.22
+
+require (
+    google.golang.org/grpc v1.55.0
+    google.golang.org/protobuf v1.30.0
+)
---
+++ server/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "context"
+    "log"
+    "net"
+
+    "github.com/yourusername/yourproject/project"
+    "google.golang.org/grpc"
+)
+
+type server struct {
+    project.UnimplementedProjectServiceServer
+}
+
+func (s *server) ListProjects(ctx context.Context, req *project.ListProjectsRequest) (*project.ListProjectsResponse, error) {
+    // TODO: データベースから案件情報を取得する処理を実装
+    projects := []*project.Project{
+        {Id: 1, Name: "Project 1", Description: "Description 1"},
+        {Id: 2, Name: "Project 2", Description: "Description 2"},
+    }
+    return &project.ListProjectsResponse{Projects: projects}, nil
+}
+
+func main() {
+    lis, err := net.Listen("tcp", ":50051")
+    if err != nil {
+        log.Fatalf("failed to listen: %v", err)
+    }
+    s := grpc.NewServer()
+    project.RegisterProjectServiceServer(s, &server{})
+    if err := s.Serve(lis); err != nil {
+        log.Fatalf("failed to serve: %v", err)
+    }
+}
